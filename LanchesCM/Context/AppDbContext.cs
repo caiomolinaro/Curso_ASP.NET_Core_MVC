d978e3ebd3b030674938a0a01e965bf7aff5976b
Caio Molinaro <cmolinaro19@gmail.com>
2023-09-21 18:16:33 -0300

    Add: EF Core, Data Annotations and Migrations

---
+++ LanchesCM/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using LanchesCM.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesCM.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {           
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Lanche> Lanches { get; set; }
+
+    }
+}
---
+++ LanchesCM/Models/Categoria.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LanchesCM.Models
+{
+    [Table("Categorias")]
+    public class Categoria
+    {
+        [Key]
+        public int CategoriaId { get; set; }
+
+        [StringLength(100, ErrorMessage ="O tamanho máximo é 100 caracteres")]
+        [Required(ErrorMessage ="Informe o nome da categoria")]
+        [Display(Name ="Nome")]
+        public string CategoriaNome { get; set; }
+
+        [StringLength(200, ErrorMessage = "O tamanho máximo é 200 caracteres")]
+        [Required(ErrorMessage = "Informe a descrição da categoria")]
+        [Display(Name = "Descrição")]
+        public string Descricao { get; set; }
+
+        public List<Lanche> Lanches { get; set; } //proprieade de navegação que vai definir que a categoria tem uma relação de 1 para muitos
+
+    }
+}
